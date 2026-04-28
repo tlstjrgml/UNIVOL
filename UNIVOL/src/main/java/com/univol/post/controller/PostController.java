b1d0d30121960815e36444f5e79ee53e462f8abb
user <user@DESKTOP-SPBEAUG>
2026-04-28 17:35:59 +0900

    봉사활동 수정

--- UNIVOL/src/main/java/com/univol/post/controller/PostController.java
+++ UNIVOL/src/main/java/com/univol/post/controller/PostController.java
@@ -1,10 +1,17 @@
 package com.univol.post.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.univol.member.model.service.MemberService;
+import com.univol.post.model.service.PostService;
+import com.univol.post.model.vo.Post;
 
+import org.springframework.ui.Model;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -12,5 +19,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 
 public class PostController {
+	private final PostService pService;
+	
+	@GetMapping("/volunteer")
+	public String selectAll(Model model) {
+		ArrayList<Post> plist = pService.selectAll();
+		System.out.println("plist: " + plist);
+		System.out.println("size: " + plist.size());
+		System.out.println("posts" + plist);
+		model.addAttribute("plist", plist);
+		
+		
+		return "post/post";
+	}
+	
 
-}
+	}
+
--- UNIVOL/src/main/java/com/univol/post/model/service/PostService.java
+++ UNIVOL/src/main/java/com/univol/post/model/service/PostService.java
@@ -1,6 +1,11 @@
 package com.univol.post.model.service;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Service;
+
+import com.univol.post.model.Mapper.PostMapper;
+import com.univol.post.model.vo.Post;
 
 import lombok.RequiredArgsConstructor;
 
@@ -8,5 +13,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 
 public class PostService {
-
+	
+	private final PostMapper mapper;
+	
+	public ArrayList<Post> selectAll(){
+		return mapper.selectAll();
+	}
 }
