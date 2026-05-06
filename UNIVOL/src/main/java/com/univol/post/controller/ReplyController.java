7ee1b6570529612acc3a832a3c9206b520b3874c
이효영 <lhy102456@gmail.com>
2026-05-06 15:39:05 +0900

    댓글 수

--- UNIVOL/src/main/java/com/univol/post/controller/ReplyController.java
+++ UNIVOL/src/main/java/com/univol/post/controller/ReplyController.java
@@ -1,14 +1,15 @@
 package com.univol.post.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import com.univol.member.model.vo.Member;
 import com.univol.post.model.service.ReplyService;
 import com.univol.post.model.vo.Reply;
-import com.univol.member.model.vo.Member;
 
 import lombok.RequiredArgsConstructor;
 
